c34f0cfe9c92e88a4d3640c061bd4f6e453c7210
Edgar <edgaras993@gmail.com>
2023-07-26 00:15:25 -0400

    sorting

---
+++ server/devData/insertCategories.js
@@ -0,0 +1,37 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const { Sequelize } = require("sequelize");
+
+const Category = require("../api/models/category");
+
+const insertCategories = async () => {
+  const sequelize = new Sequelize(process.env.DB_CONN, {
+    logging: console.log,
+  });
+  try {
+    await sequelize.authenticate();
+    console.log("connected");
+
+    const categories = [
+      { name: "subscriptions" },
+      { name: "groceries" },
+      { name: "household" },
+      { name: "utilities" },
+      { name: "income" },
+      { name: "savings" },
+      { name: "transportation" },
+      { name: "healthcare" },
+      { name: "education" },
+      { name: "charity" },
+      { name: "misc" },
+      { name: "food" },
+      { name: "clothing" },
+    ];
+    const createdCategories = await Category.bulkCreate(categories);
+    console.log(createdCategories);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+insertCategories();
